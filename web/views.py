c09162868d987216c6198629cb1b097f6cd6f34a
Ever <informaticoebz@gmail.com>
2025-06-21 14:09:07 -0300

    diseño index

--- web/views.py
+++ web/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 
+from .models import Categoria,Producto
+
 # Create your views here.
 """ Vistas para el catalogo de productos """
 def index(request):
-    return render(request, 'index.html')
+    listaProductos = Producto.objects.all()
+    context = {
+        'productos' : listaProductos
+    }
+    return render(request, 'index.html', context)
