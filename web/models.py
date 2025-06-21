8e8daaa2dc5f2dfd4008da7f4a01bfae01d55645
Ever <informaticoebz@gmail.com>
2025-06-21 15:28:02 -0300

    trabajo en filtrado

--- web/models.py
+++ web/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+# Create your models here.
 class Categoria(models.Model):
     nombre = models.CharField(max_length=200)
     fecha_registro = models.DateTimeField(auto_now_add=True)
@@ -7,11 +8,10 @@ class Categoria(models.Model):
     def __str__(self):
         return self.nombre
     
-    
 class Producto(models.Model):
-    categoria = models.ForeignKey(Categoria, on_delete=models.RESTRICT)
+    categoria = models.ForeignKey(Categoria,on_delete=models.RESTRICT)
     nombre = models.CharField(max_length=200)
-    descripcion = models.TextField(null=True)
+    descripcion= models.TextField(null=True)
     precio = models.DecimalField(max_digits=9,decimal_places=2)
     fecha_registro = models.DateTimeField(auto_now_add=True)
     imagen = models.ImageField(upload_to='productos',blank=True)
--- web/views.py
+++ web/views.py
@@ -6,7 +6,23 @@ from .models import Categoria,Producto
 """ Vistas para el catalogo de productos """
 def index(request):
     listaProductos = Producto.objects.all()
+    listaCategorias = Categoria.objects.all()
     context = {
-        'productos' : listaProductos
+        'productos' : listaProductos,
+        'categorias' : listaCategorias
     }
     return render(request, 'index.html', context)
+
+def productosPorCategoria(request,categoria_id):
+    """ vista para filtrar productos por categoria"""
+    objCategoria = Categoria.objects.get(pk=categoria_id)
+    listaProductos = objCategoria.producto_set.all()
+    
+    listaCategorias = Categoria.objects.all()
+    
+    context = {
+        'categorias':listaCategorias,
+        'productos':listaProductos
+    }
+    
+    return render(request, 'index.html')
