bcb8aa94b42b59514242f0d8614e24e1cc044aa6
Ever <informaticoebz@gmail.com>
2025-06-25 16:39:07 -0300

    hasta aqui ya se puede usar con whatsapp

--- web/models.py
+++ web/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Categoria(models.Model):
@@ -18,3 +19,41 @@ class Producto(models.Model):
     
     def __str__(self):
         return self.nombre
+    
+
+#desde aqui no me sirve para usar pedido directo por whatsapp
+class Cliente(models.Model):
+    usuario = models.OneToOneField(User,on_delete=models.RESTRICT)
+    cdi = models.CharField(max_length=10)
+    sexo = models.CharField(max_length=1,default='M')
+    telefono = models.CharField(max_length=20)
+    fecha_nacimiento = models.DateField(null=True)
+    direccion = models.TextField()
+    
+    def __str__(self):
+        return self.cdi
+    
+class Pedido(models.Model):
+    
+    ESTADO_CHOICES = (
+        ('0','Solicitado'),
+        ('1','Pagado')
+    )
+    
+    cliente = models.OneToManyField(Cliente,on_delete=models.RESTRICT)
+    fecha_registro = models.DateTimeField(auto_now_add=True)
+    nro_pedido = models.CharField(max_length=20,null=True)
+    monto_total = models.DecimalField(max_digits=10,decimal_places=2,default=0)
+    estado = models.CharField(max_length=1,default='0',choices=ESTADO_CHOICES)
+    
+    def __str__(self):
+        return self.nro_pedido
+    
+class PedidoDetalle(models.Model):
+    pedido = models.OneToManyField(Pedido,on_delete=models.RESTRICT)
+    producto = models.OneToManyField(Producto,on_delete=models.RESTRICT)
+    cantidad = models.IntegerField(default=1)
+    subtotal = models.DecimalField(max_digits=10,decimal_places=2)
+    
+    def __str__(self):
+        return self.producto.nombre
